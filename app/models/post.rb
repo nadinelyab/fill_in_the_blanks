7cbf180fe848b0908dfbca66e417be664add4e6d
nadinelyab <nadinelyab@gmail.com>
2015-07-23 16:33:43 +0200

    Done with this app

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ActiveRecord::Base
+	validates :bru_name, :title, :body, presence: true
 	# create some validations here that ensure both our attributes
 	# are present when a user creates or edits a post
 end
